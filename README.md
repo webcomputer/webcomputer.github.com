930196a3a4de19b2fac0faac7295ac8e2ede4534
Jonas Almeida <jalmeida@mathbiol.org>
2017-03-02 10:27:52 -0500

    live link

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # webcomputer.github.com
 book root
+___
+
+Book live at [webcomputer.github.io](https://webcomputer.github.io) !
+
+webComputer library at https://webcomputer.github.io/webComputer.js,
+
+```
+<script src="https://webcomputer.github.io/webComputer.js"><script>
+```
--- webComputer.js
+++ webComputer.js
@@ -3,15 +3,16 @@ console.log('webComputer loaded ...');
 webComputer=function(){
     console.log('ini')
     if(document.getElementById('webComputerDiv')){
-        webComputer.bookCover(document.getElementById('webComputerDiv'))
+        webComputer.book(document.getElementById('webComputerDiv'),location.href+'/html/intro.html')
     }
 }
 
 
-webComputer.bookCover=function(div){
-    var h = '<h1 style="color:maroon">Data Science for<br> the WebComputer</h1>'
-    h +='<h5><i>Jonas S Almeida, March 2017 - ...</i></h4>'
-    div.innerHTML=h
+webComputer.book=function(div,path){
+    $.get(path)
+     .then(function(h){
+         div.innerHTML=h
+     })
 }
 
 
