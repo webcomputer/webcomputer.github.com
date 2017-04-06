95b8328f9a795aef2b5466083a6dbc69b635c05d
Jonas Almeida <jalmeida@mathbiol.org>
2017-04-06 17:16:13 -0400

    preparing for health IT

--- webComputer.js
+++ webComputer.js
@@ -28,18 +28,27 @@ webComputer.refs=function(div){ // references ref/<fn>.json
     divRefs.appendChild(ol)
     var rr = []
     for(var i=0 ; i<refs.length ; i++){rr.push(refs[i])} // silly DOM prefers a non Array for selectors ... 
+    // index ids
+    var ind={},i=1
+    rr.forEach(function(r){
+        if(!ind[r.id]){
+            ind[r.id]=i
+            i++
+            var li = document.createElement('li')
+            ol.appendChild(li)
+            $.getJSON('ref/'+r.id+'.json')
+             .then(function(ri){
+                 if(ri.url){
+                     ri.html+=' <a href="'+ri.url+'" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a>.'
+                 }
+                 li.innerHTML=ri.html
+             })
+        }
+    })
+    // replace each ref by index
     rr.forEach(function(r,i){
-        r.textContent='['+(i+1)+']'
+        r.textContent='['+ind[r.id]+']' // add links here at some point
         r.style.color='blue'
-        var li = document.createElement('li')
-        ol.appendChild(li)
-        $.getJSON('ref/'+r.id+'.json')
-         .then(function(ri){
-             if(ri.url){
-                 ri.html+=' <a href="'+ri.url+'" target="_blank"><i class="fa fa-external-link" aria-hidden="true"></i></a>.'
-             }
-             li.innerHTML=ri.html
-         })
     })
 }
 
